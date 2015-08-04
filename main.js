4a0492dc4e04c6f5aa4dd75911f581c9e2d8fe3c
Arash Dai <arashdai@gmail.com>
2015-08-04 10:12:56 -0700

    updated js files

---
+++ main.js
@@ -0,0 +1,108 @@
+var food ={
+	hamburger:{
+		calories:390 ,
+		totalFat:19,
+		transFat:0 ,
+		satFat:5 ,
+		carbs:39 ,
+		fiber:3 ,
+		sugar:10 ,
+		protein:16 ,
+		sodium:650
+},	
+	cheeseburger:{
+		calories:480 ,
+		totalFat:27 ,
+		transFat:0.5 ,
+		satFat:10 ,
+		carbs:39 ,
+		fiber:3 ,
+		sugar:10 ,
+		protein:22 ,
+		sodium:1000
+},
+
+	doubleDouble:{
+		calories:670 ,
+		totalFat:41 ,
+		transFat:1 ,
+		satFat:18 ,
+		carbs:39,
+		fiber:3 ,
+		sugar:11 ,
+		protein:37 ,
+		sodium:1440
+},
+
+	fries:{
+		calories:395 ,
+		totalFat:18 ,
+		transFat:0 ,
+		satFat:5 ,
+		carbs:54 ,
+		fiber:2 ,
+		sugar:0 ,
+		protein:7 ,
+		sodium:245
+},
+
+	smallSoda:{
+		calories: ,
+		totalFat: ,
+		transFat: ,
+		satFat: ,
+		carbs: ,
+		fiber: ,
+		sugar: ,
+		protein: ,
+		sodium:
+},
+	mediumSoda:{
+		calories: ,
+		totalFat: ,
+		transFat: ,
+		satFat: ,
+		carbs: ,
+		fiber: ,
+		sugar: ,
+		protein: ,
+		sodium:
+},
+	largeSoda:{
+		calories: ,
+		totalFat: ,
+		transFat: ,
+		satFat: ,
+		carbs: ,
+		fiber: ,
+		sugar: ,
+		protein: ,
+		sodium:
+},
+	milkshake:{
+		calories:590,
+		totalFat:29 ,
+		transFat:1 ,
+		satFat:19 ,
+		carbs:72 ,
+		fiber:0 ,
+		sugar:65 ,
+		protein:10 ,
+		sodium:320
+}
+
+
+};
+
+var RMI ={
+calories:2000,	
+totalFat:70,
+transFat:0.01,
+satFat:24,
+carbs:310,
+fiber:28,
+sugar:90,
+protein:50,
+sodium:2300
+
+}
